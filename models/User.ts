de846dfcaa840da8f9258cffa086ee927a8f3108
Shivani <shivaniyadav6705@gmail.com>
2026-07-03 17:57:11 +0530

    Fix location API route

--- models/User.ts
+++ models/User.ts
@@ -32,9 +32,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 UserSchema.methods.comparePassword = async function(password: string) {
